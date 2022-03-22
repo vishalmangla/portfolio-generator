f23b9a2109a047f767a3dc4213d900f23a120456
Vishal Mangla <mangla@gmail.com>
2022-03-22 11:38:49 -0700

    index file getting generated

--- app.js
+++ app.js
@@ -1,37 +1,11 @@
-//var commandLineArgs = process.argv.slice(2, process.argv.length);
-//console.log(commandLineArgs);
-
-const animalArray = ['dog', 'cat', 'pig'];
-
-animalArray.push('cow');
-//console.log(animalArray);
-
-const personObj = {
-  name: 'Lernantino',
-  age: 99
-};
-
-personObj.age = 100;
-personObj.occupation = 'Developer';
-//console.log(personObj);
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
+const fs = require (`fs`);
+const generatePage = require("./src/page-template");
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
 
-  console.log('================');
-
-  // Is the same as this...
-  //profileDataArr.forEach((profileItem) => {
-    //console.log(profileItem)
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-  
-};
-printProfileData(profileDataArgs);
-
+fs.writeFile('./index.html', generatePage(name,github), err => {
+  if (err) throw new Error (err);
+  console.log('Portfolio done check index.html');
+});
