f9d251f02b9845e66b8829b5ae05992db4df1971
Vishal Mangla <mangla@gmail.com>
2022-03-24 17:50:08 -0700

    Inquirer working

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const fs = require (`fs`);
-const generatePage = require("./src/page-template");
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require(`inquirer`);
+// const fs = require (`fs`);
+// const generatePage = require("./src/page-template");
+// const pageHTML = generatePage(name,github);
 
-
-
-fs.writeFile('./index.html', generatePage(name,github), err => {
-  if (err) throw new Error (err);
-  console.log('Portfolio done check index.html');
-});
-
-const inquirer = require('inquirer');
-console.log(inquirer);
+// fs.writeFile("./index.html", pageHTML, err => {
+//   if (err) throw err;
+//   console.log('Portfolio complete check html');
+// });
+inquirer.prompt ([
+  {
+    type:'input',
+    name: 'Name',
+    message:'Whats is your name?'
+  }
+])
+.then (answers => console.log(answers));
